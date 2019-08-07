9e2f7b572a5d10c36a16e8bb1e782f0bd55a6e20
Saurabh suman <42254624+saurabh98s@users.noreply.github.com>
2019-08-07 18:47:21 +0530

    Update image_pro.py

--- image_pro.py
+++ image_pro.py
@@ -1,35 +1,13 @@
-from PIL import Image
-import numpy as np
-import matplotlib.pyplot as plt
-import time
-from statistics import mean
-def threshold(imageArray):
-    balanceAr = []
-    newAr = imageArray
-    for eachRow in imageArray:
-        for eachPix in eachRow:
-            avgNum = mean(eachPix[:3])
-            balanceAr.append(avgNum)
-i = Image.open('images/numbers/0.1.png')
-iar = np.array(i)
-i2 = Image.open('images/numbers/y0.4.png')
-iar2 = np.array(i2)
-i3 = Image.open('images/numbers/y0.5.png')
-iar3 = np.array(i3)
-i4 = Image.open('images/sentdex.png')
-iar4 = np.array(i4)
+from imageai.Detection import ObjectDetection
+import os
 
+execution_path = os.getcwd()
 
-fig = plt.figure()
-ax1 = plt.subplot2grid((8,6),(0,0), rowspan=4, colspan=3)
-ax2 = plt.subplot2grid((8,6),(4,0), rowspan=4, colspan=3)
-ax3 = plt.subplot2grid((8,6),(0,3), rowspan=4, colspan=3)
-ax4 = plt.subplot2grid((8,6),(4,3), rowspan=4, colspan=3)
+detector = ObjectDetection()
+detector.setModelTypeAsRetinaNet()
+detector.setModelPath( os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5"))
+detector.loadModel()
+detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "image3.jpg"), output_image_path=os.path.join(execution_path , "imagenew.jpg"))
 
-ax1.imshow(iar)
-ax2.imshow(iar2)
-ax3.imshow(iar3)
-ax4.imshow(iar4)
-
-
-plt.show()    
+for eachObject in detections:
+    print(eachObject["name"] , " : " , eachObject["percentage_probability"] )
